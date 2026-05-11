cd3c1c703da1c2c7d2b04b84b017e79f7b5476cb
Vinicius <viniciusandrade0521@gmail.com>
2026-05-11 10:28:15 -0300

    feat: kickoff

---
+++ formulario/src/components/Cabecalho.jsx
@@ -0,0 +1,10 @@
+function Cabecalho() {
+    return (
+        <header>
+            <h1>Primeiro Componente</h1>
+            <p>Header bonitão, ou não...</p>
+        </header>
+    )
+}
+
+export default Cabecalho
---
+++ formulario/src/components/letreiro.jsx
@@ -0,0 +1,13 @@
+function Letreiro() { 
+  return ( 
+    <header>
+      <marquee> 
+        <h1>Atenção: Super Descontos na Nova Coleção Marqueen! Aproveite o frete grátis hoje!</h1> 
+      </marquee> 
+      <marquee> Novidades exclusivas toda semana! </marquee> 
+    </header>
+  ); 
+};
+
+
+export default Letreiro;
