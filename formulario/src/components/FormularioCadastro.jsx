8cb125c690e7452556d91a2f5558f8994e196d65
Vinicius <viniciusandrade0521@gmail.com>
2026-05-12 11:40:43 -0300

    fix: valuer and charge

--- formulario/src/components/FormularioCadastro.jsx
+++ formulario/src/components/FormularioCadastro.jsx
@@ -1,14 +1,43 @@
 import InputField from "./InputField"; // Importação correta
+import { useState } from "react"; // Importação do useState
 
-function Formulario() { 
+function Formulario() {
+  const [nome, setnome] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
   return (
     <form>
-      <InputField label="Nome" type="text" name="nome" placeholder={} />
-      <InputField label="Email" type="email" name="email" />
-      <InputField label="Senha" type="password" name="senha" />
-      <InputField label= "checkbox" type="checkbox" name="termos" />
-      <InputField label="date" type="date" name="dataNascimento" />
-      <InputField label="file" type="file" name="arquivo" />
+      <InputField label="Nome" 
+        type="text" name="nome" 
+        placeholder={"nome"} 
+        value={nome} 
+        onChange={(e) => setnome(e.target.value)} />
+      <InputField 
+        label="Email" 
+        type="email" 
+        name="email" 
+        placeholder={"email"} 
+        value={email} 
+        onChange={(e) => setEmail(e.target.value)} />
+      <InputField 
+        label="Telefone" 
+        type="tel" 
+        name="telefone" 
+        placeholder={"telefone"} 
+        value={telefone} 
+        onChange={(e) => setTelefone(e.target.value)} />
+      <InputField 
+        label= "checkbox" 
+        type="checkbox" 
+        name="termos" />
+      <InputField 
+        label="date" 
+        type="date" 
+        name="dataNascimento" />
+      <InputField 
+        label="file" 
+        type="file" 
+        name="arquivo" />
     </form> 
   );
 }
