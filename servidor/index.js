85a1dedf4aa2615e35c0cecc7c9a6234f8d741d2
Vinicius <viniciusandrade0521@gmail.com>
2026-05-15 10:53:04 -0300

    feat: servidor rodas serve

--- servidor/index.js
+++ servidor/index.js
@@ -1,10 +1,41 @@
-import express from "express";
-import 
+import express from 'express';
 
-const app = express();
-app.use()
+const servidor = express();
+
+servidor.use(express.json())
+
+const registro = [] // """banco de dados""" 
+
+servidor.post('/registros', (req,res) => {
+  const dados = req.body // pega o corpo da requisição
+
+  if (!dados.nome) {
+    res.status(400).json({
+      mensagem: 'campo nome é obrigatório!'
+    })
+  }
+
+  console.log(`Dados da requsição!
+     o que tem no corpo que o frontend me mandou:
+    ${dados}`)
+    registro.push(dados) // simulação de guardar dados no banco
+  
+    res.status(201).json({
+      mensagem: 'Dados recebidos com sucesso'
+    })
+});
 
 
-app.listen(3000, () => {
+
+
+
+
+
+
+
+servidor.get('/registro', (req, res) => {
+  res.status(200).json(registro);
+});
+servidor.listen(3000, () => {
   console.log("Servidor rodando http://localhost:3000");
 });
